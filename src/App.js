3f2ad4e1f670f123108edf1ba2d9e184556c7d7c
Ishant713 <ishandhakad65@gmail.com>
2026-02-24 16:38:25 +0530

    Fix useEffect dependency properly

--- src/App.js
+++ src/App.js
@@ -3,27 +3,22 @@ import { useEffect } from 'react';
 import Input from './Components/Input';
 import { Button } from './Components/Button';
 import { Card } from './Components/Card';
-
-
 import { useWeather } from './context/Weather';
+
 function App() {
-  const Weather =useWeather();
 
-useEffect(() => {
-  Weather.fetchCurrentUserLocationData();
-}, [Weather.fetchCurrentUserLocationData]);
+  const { fetchCurrentUserLocationData, fetchData } = useWeather();
 
+  useEffect(() => {
+    fetchCurrentUserLocationData();
+  }, [fetchCurrentUserLocationData]);
 
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
       <Input/>
-      <Button className="search-box" onClick={Weather.fetchData} value="Search"/>
+      <Button className="search-box" onClick={fetchData} value="Search"/>
       <Card/>
-      {/* <Button value="Referesh"/> */}
-
-
-
     </div>
   );
 }
